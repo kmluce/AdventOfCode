41aba8b4cdb58533c6baea20bb1f503ae943b6d9
Kathleen Luce <kmluce@gmail.com>
2020-12-09 20:29:58 -0800

    Added day 2, first problem translated from Perl.

---
+++ day2_a.py
@@ -0,0 +1,30 @@
+import fileinput
+
+def parse_line(line):
+    splitLine = line.split()
+    print(splitLine)
+    srrange = splitLine[0].split('-')
+    return [ int(srrange[0]), int(srrange[1]), splitLine[1][0], splitLine[2] ]
+
+def check_string(myArgs):
+    low = myArgs[0]
+    high = myArgs[1]
+    matchChar = myArgs[2]
+    matchString = myArgs[3]
+
+    matches = matchString.count(matchChar)
+    if matches >= low and matches <= high:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    validCount = 0
+    for line in fileinput.input():
+        line = line.rstrip()
+        parsedLine = parse_line(line)
+        print(" rangelow: ", parsedLine[0], " rangehigh: ", parsedLine[1], " char: ", parsedLine[2],
+              " string: ", parsedLine[3])
+        if check_string(parsedLine):
+            validCount += 1
+    print("Number of valid passwords is: ", validCount)

6c7579172db0e3da443146702ad687f491ac3d09
Kathleen Luce <kmluce@gmail.com>
2020-12-13 16:19:28 -0800

    Day 11, problem 1 completed

--- day3_a.py
+++ day3_a.py
@@ -11,6 +11,19 @@ class TobogganMap:
     def print_map(self):
         print('\n'.join(self.map))
 
+    def check_map(self, step_x, step_y):
+        x = 0
+        total_trees=0
+        for y in range(len(self.map), step_y):
+            if self.map[y][x] == '#':
+                print(" tree at ", y, ",", x)
+                total_trees += 1
+            
+
+
+
+
+
 #TODO finish rest of problem
 
 if __name__ == '__main__':

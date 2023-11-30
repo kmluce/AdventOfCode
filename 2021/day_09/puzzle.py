1dd8b18f5e90c8b659bc7fa4a0ee8ca9f8b9cccc
luce <kmluce@gmail.com>
2023-11-30 10:37:01 -0800

    start on day 9 part b

--- 2021/day_09/puzzle.py
+++ 2021/day_09/puzzle.py
@@ -5,6 +5,10 @@ class Puzzle:
         self.fileName = file_name
         self.tubeMap = []
         self.riskLevelSum = 0
+        self.lowest_points = []
+        self.visited = []
+        self.tmp_basin_size = 0
+        self.basin_sizes = []
 
     def parse(self):
         with open(self.fileName) as file:
@@ -14,13 +18,13 @@ class Puzzle:
 
     def find_risk(self, x, y):
         found_lower = 0
-        for test_y in range(-1,2):
+        for test_y in range(-1, 2):
             curr_y = y + test_y
-            if (curr_y < 0) or (curr_y > len(self.tubeMap)-1):
+            if (curr_y < 0) or (curr_y > len(self.tubeMap) - 1):
                 continue
-            for test_x in range(-1,2):
+            for test_x in range(-1, 2):
                 curr_x = x + test_x
-                if (curr_x < 0) or (curr_x > len(self.tubeMap[y])-1):
+                if (curr_x < 0) or (curr_x > len(self.tubeMap[y]) - 1):
                     continue
                 elif abs(test_x) == abs(test_y):
                     continue
@@ -29,16 +33,47 @@ class Puzzle:
                     # print("       and it is", self.tubeMap[curr_y][curr_x])
                     if self.tubeMap[curr_y][curr_x] <= self.tubeMap[y][x]:
                         found_lower = found_lower + 1
-                    if (found_lower > 0):
+                    if found_lower > 0:
                         break
-            if(found_lower > 0):
+            if found_lower > 0:
                 break
         if found_lower == 0:
-            return (self.tubeMap[y][x] + 1)
+            self.lowest_points.append([x, y])
+            return self.tubeMap[y][x] + 1
         else:
             return 0
 
-    def solvea(self):
+    def find_basin_size(self, point):
+        x = point[0]
+        y = point[1]
+        self.tmp_basin_size = 0
+        self.visited = [point]
+        self.get_basin_recursively(x, y)
+        self.basin_sizes.append(self.tmp_basin_size)
+
+    def get_basin_recursively(self, x, y):
+        for test_y in range(-1, 2):
+            curr_y = y + test_y
+            if (curr_y < 0) or (curr_y > len(self.tubeMap) - 1):
+                continue
+            for test_x in range(-1, 2):
+                curr_x = x + test_x
+                if (curr_x < 0) or (curr_x > len(self.tubeMap[y]) - 1):
+                    continue
+                elif abs(test_x) == abs(test_y):
+                    continue
+                elif [curr_x, curr_y] in self.visited:
+                    continue
+                else:
+                    # print("  checking", curr_x, ",", curr_y)
+                    # print("       and it is", self.tubeMap[curr_y][curr_x])
+                    self.visited.append([curr_x, curr_y])
+                    if self.tubeMap[curr_y][curr_x] < 9:
+                        self.tmp_basin_size += 1
+                        self.get_basin_recursively(curr_x, curr_y)
+        return
+
+    def solve_a(self):
         total_risk = 0
         # print("Initial map is")
         for line in self.tubeMap:
@@ -48,3 +83,15 @@ class Puzzle:
                 # print("evaluating", x, y, "which is", self.tubeMap[y][x])
                 total_risk = total_risk + self.find_risk(x, y)
         return total_risk
+
+    def solve_b(self):
+        for line in self.tubeMap:
+            print("  ", line)
+        for y, line in enumerate(self.tubeMap):
+            for x, height in enumerate(line):
+                # print("evaluating", x, y, "which is", self.tubeMap[y][x])
+                self.find_risk(x, y)
+        for point in self.lowest_points:
+            print("finding basin for point", point)
+            self.find_basin_size(point)
+        print(self.basin_sizes.sort())

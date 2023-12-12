42706d6b6cdbf9488b6bdbb9a8504728fd9ccb79
luce <kmluce@gmail.com>
2023-12-11 16:19:19 -0800

    Day 4, part 2 completed

--- 2023/day_04/puzzle.py
+++ 2023/day_04/puzzle.py
@@ -12,6 +12,8 @@ class Puzzle:
         self.total_points = 0
         self.cards_owned = []
         self.card_copies = []
+        self.num_cards_won_round = []
+        self.list_cards_won_round = []
 
     def print_run_info(self):
         self.debug.print(1, f"{chr(10)}PUZZLE RUN:  running part {self.puzzle_part} with file "
@@ -33,12 +35,23 @@ class Puzzle:
                     if winning_numbers.intersection(my_numbers):
                         self.total_points += pow(2, len(winning_numbers.intersection(my_numbers)) - 1)
                         self.card_copies.append(1)
-                        self.cards_owned.append([])
+                        self.num_cards_won_round.append(len(winning_numbers.intersection(my_numbers)))
+                    else:
+                        self.card_copies.append(1)
+                        self.num_cards_won_round.append(0)
                 else:
                     pass
 
-    # def calculate_cards_owned(self):
-    #     for card_number in range(len(self.card_copies))
+    def calculate_cards_owned(self):
+        self.list_cards_won_round = [None] * (len(self.num_cards_won_round))
+        self.debug.print(1, "")
+        for index, num_cards in reversed(list(enumerate(self.num_cards_won_round))):
+            self.debug.print(3, f"card {index} won {num_cards} cards")
+            self.list_cards_won_round[index] = [index]
+            for i in range(0, num_cards):
+                self.list_cards_won_round[index].extend(self.list_cards_won_round[index + i + 1])
+        self.debug.print(2, f"{self.list_cards_won_round}")
+        return len([x for sublist in self.list_cards_won_round for x in sublist])
 
     def solve(self):
         self.parse()
@@ -46,4 +59,4 @@ class Puzzle:
         if self.puzzle_part == "a":
             return self.total_points
         else:
-            return 0
+            return self.calculate_cards_owned()

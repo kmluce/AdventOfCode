c3d859a1f3dd73ec15032a91557e2aa61a176f84
kmluce <kmluce@gmail.com>
2024-11-30 20:53:54 -0800

    Just the example files for 2023, day 10

---
+++ 2023/day_10/puzzle.py
@@ -0,0 +1,34 @@
+from utils.debugging import PrintDebug
+
+
+class Puzzle:
+    fileName: str
+
+    def __init__(self, file_name, puzzle_part, debug_level):
+        self.fileName = file_name
+        self.puzzle_part = puzzle_part
+        self.debug = PrintDebug(debug_level)
+        self.debug_level = debug_level
+
+    def print_run_info(self):
+        self.debug.print(1, f"{chr(10)}PUZZLE RUN:  running part {self.puzzle_part} with file "
+                            f"{self.fileName} and debug level {self.debug_level}")
+
+    def print(self):
+        pass
+
+    def parse(self):
+        with open(self.fileName) as file:
+            for line in file:
+                if line.rstrip():
+                    line = line.rstrip()
+                else:
+                    pass
+
+    def solve(self):
+        self.parse()
+        self.print()
+        if self.puzzle_part == "a":
+            return 0
+        else:
+            return 0

a5e1effe78ac80ae5d2cc180cdc7441fbc97fcf5
kmluce <kmluce@gmail.com>
2024-12-04 22:47:53 -0800

    solved 2024 day 5, part 2 is in progress

--- 2024/day_05/puzzle.py
+++ 2024/day_05/puzzle.py
@@ -13,6 +13,9 @@ class Puzzle:
         self.pages_list = []
         self.pages_dict = {}
         self.page_sum = 0
+        self.rule_breakers = []
+        self.left_set = set()
+        self.right_set = set()
 
     def print_run_info(self):
         self.debug.print(1, f"{chr(10)}PUZZLE RUN:  running part {self.puzzle_part} with file "
@@ -29,6 +32,8 @@ class Puzzle:
                     if '|' in line:
                         first, last = line.split('|')
                         self.rules.append([int(first), int(last)])
+                        self.left_set.add(int(first))
+                        self.right_set.add(int(last))
                     else:
                         self.debug.print(3, f"rule list looks like: {self.rules}")
                         self.pages_dict = {}
@@ -60,6 +65,32 @@ class Puzzle:
             self.debug.print(3, f"index {page_list} is a rule follower")
             middle_index = int((len(page_list) - 1) / 2)
             self.page_sum += int(page_list[middle_index])
+        else:
+            self.rule_breakers.append(page_list)
+
+    def fix_rule_breakers(self):
+        left_rule_pages = []
+        right_rule_pages = []
+        for page in self.left_set:
+            left_rule_pages[page] = set([x for x in self.rules[1] if self.rules[0] == page])
+        for page in self.right_set:
+            right_rule_pages[page] = set([x for x in self.rules[0] if self.rules[1] == page])
+        for page_list in self.rule_breakers:
+            self.debug.print(1, f"current rule breaker is {page_list}")
+            tmp_page_list = []
+            for current_page in page_list:
+                self.debug.print(1, f"inserting page {current_page} into list {tmp_page_list}")
+                if len(tmp_page_list) == 0:
+                    tmp_page_list.append(current_page)
+                else:
+                    for split in range(len(tmp_page_list)):
+                        self.debug.print(1, f"splitting at {split}")
+                        if (set(tmp_page_list[0:split]).isdisjoint(left_rule_pages[current_page])
+                            and set(tmp_page_list[split:-1]).isdisjoint(right_rule_pages[current_page])):
+                            tmp_page_list.insert(split, current_page)
+                self.debug.print(1, f"fully inserted list is {tmp_page_list}")
+
+        return 0
 
     def solve(self):
         self.parse()
@@ -67,4 +98,4 @@ class Puzzle:
         if self.puzzle_part == "a":
             return self.page_sum
         else:
-            return 0
+            return self.fix_rule_breakers()

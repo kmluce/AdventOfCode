8554c6be61af3384a5e733dbd9ae584422aa1f9c
kmluce <kmluce@gmail.com>
2024-12-04 17:32:54 -0800

    solved day 4

--- 2024/day_04/puzzle.py
+++ 2024/day_04/puzzle.py
@@ -1,4 +1,5 @@
 from utils.debugging import PrintDebug
+import re
 
 
 class Puzzle:
@@ -13,6 +14,8 @@ class Puzzle:
         self.vertical_search_field = []
         self.left_diagonal = []
         self.right_diagonal = []
+        self.xmas_re = re.compile("XMAS")
+        self.total_xmas = 0
 
 
     def print_run_info(self):
@@ -31,20 +34,132 @@ class Puzzle:
                 else:
                     pass
 
+    # I made a lot of amusing work for myself by wanting to handle this with list comprehensions
+    # It required me to learn to rotate a matrix so that the diagonals were rows
+    # This is a skill that's probably not necessary for many real-life problems
     def splice_lists(self):
         tmp_list = [[m[i] for m in self.search_field] for i in range(len(self.search_field[0]))]
         self.vertical_search_field = [''.join(a) for a in tmp_list]
         print("original search field:")
-        print(self.search_field)
+        self.print_array(self.search_field)
         print()
-        print("rotated search field:")
-        print(self.vertical_search_field)
+        print("vertical search field:")
+        self.print_array(self.vertical_search_field)
+        print()
+        print("vertical search field:")
+        self.right_diagonal = self.make_right_diagonal(self.search_field)
+        self.print_array(self.right_diagonal)
+        print()
+        print("vertical search field:")
+        self.left_diagonal = self.make_left_diagonal(self.search_field)
+        self.print_array(self.left_diagonal)
+        print()
+
+    def make_left_diagonal(self, matrix):
+        new_matrix = []
+        height = len(matrix)
+        width = len(matrix[0])
+        for k in range(width + height - 1):
+            new_matrix.append('')
+            for j in range(k + 1):
+                i = k - j
+                if j < height and i < width:
+                    self.debug.print(3, f"appending {matrix[j][i]} to new_matrix[{k}]")
+                    new_matrix[k] += matrix[j][i]
+        return new_matrix
+
+    def make_right_diagonal(self, matrix):
+        new_matrix = []
+        height = len(matrix)
+        width = len(matrix[0])
+        for k in range(width + height - 1):
+            new_matrix.append('')
+            for j in range(k + 1):
+                i = k - j
+                if j < height and i < width:
+                    self.debug.print(3, f"appending {matrix[j][width - i - 1]} to new_matrix[{k}]")
+                    new_matrix[k] += matrix[j][width-i-1]
+        return new_matrix
+
+    def test_splices(self):
+        test = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l']]
+        print("original test:")
+        self.print_array(test)
+        print()
+        print("vertical test:")
+        tmp_list = [[m[i] for m in test] for i in range(len(test[0]))]
+        self.print_array(tmp_list)
+        print()
+        print("original test:")
+        self.print_array(test)
+        print()
+        print("left diagonal test:")
+        self.print_array(self.make_left_diagonal(test))
+        print()
+        print("right diagonal test:")
+        self.print_array(self.make_right_diagonal(test))
+
+    @staticmethod
+    def print_array(array):
+        print(array)
+        for i in range(len(array)):
+            for j in range(len(array[i])):
+                print(array[i][j], end=" ")
+            print()
+
+    def find_all_words(self):
+        instances_of_xmas = [ len(self.xmas_re.findall(line)) for line in self.search_field]
+        print("searching for horizontal words in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+        instances_of_xmas = [ len(self.xmas_re.findall(line[::-1])) for line in self.search_field]
+        print("searching for horizontal words backwards in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+
+        instances_of_xmas = [ len(self.xmas_re.findall(line)) for line in self.vertical_search_field]
+        print("searching for vertical words in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+        instances_of_xmas = [ len(self.xmas_re.findall(line[::-1])) for line in self.vertical_search_field]
+        print("searching for vertical words backwards in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+
+        instances_of_xmas = [ len(self.xmas_re.findall(line)) for line in self.right_diagonal]
+        print("searching for right diagonal words in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+        instances_of_xmas = [ len(self.xmas_re.findall(line[::-1])) for line in self.right_diagonal]
+        print("searching for right diagonal words backwards in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+
+        instances_of_xmas = [ len(self.xmas_re.findall(line)) for line in self.left_diagonal]
+        print("searching for left diagonal words in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+        instances_of_xmas = [ len(self.xmas_re.findall(line[::-1])) for line in self.left_diagonal]
+        print("searching for left diagonal words backwards in search field:", instances_of_xmas)
+        self.total_xmas += sum(instances_of_xmas)
+
+        print("total instances of XMAS:", self.total_xmas)
+
+        return self.total_xmas
+
+    def solve_part_b(self):
+        for row in range(1, len(self.search_field) - 1):
+            index_tmp = [ i for i, ltr in enumerate(self.search_field[row]) if ltr == "A"]
+            # need to exclude the first and last columns
+            indices_of_a = [index for index in index_tmp if 0 < index < len(self.search_field[row]) - 1]
+            for index in indices_of_a:
+                if ((self.search_field[row-1][index-1] == "M" and self.search_field[row+1][index+1] == "S")
+                    or (self.search_field[row - 1][index - 1] == "S" and self.search_field[row + 1][index + 1] == "M")):
+                    if ((self.search_field[row - 1][index + 1] == "M" and self.search_field[row + 1][index - 1] == "S")
+                        or (self.search_field[row - 1][index + 1] == "S"
+                            and self.search_field[row + 1][index - 1] == "M")):
+                                self.total_xmas += 1
+
 
     def solve(self):
         self.parse()
         self.print()
         if self.puzzle_part == "a":
             self.splice_lists()
-            return 0
+            return self.find_all_words()
         else:
-            return 0
+            self.solve_part_b()
+            return self.total_xmas
--- 2024/day_04/test_puzzle.py
+++ 2024/day_04/test_puzzle.py
@@ -3,29 +3,29 @@ from puzzle import Puzzle
 
 
 class MyTestCase(unittest.TestCase):
-    # @unittest.skip("Skipping testing part A with demo data, not yet implemented")
+    @unittest.skip("Skipping testing part A with demo data, not yet implemented")
     def test_demo_part_a(self):
         demo_a = Puzzle("demo_data1.txt", "a", 1)
         answer = demo_a.solve()
-        self.assertEqual(-1, answer)
+        self.assertEqual(18, answer)
 
     @unittest.skip("Skipping testing part A with real data, not yet implemented")
     def test_part_a(self):
         part_a = Puzzle("test_data.txt", "a", 1)
         answer = part_a.solve()
-        self.assertEqual(-1, answer)
+        self.assertEqual(2613, answer)
 
-    @unittest.skip("Skipping testing part B with demo data, not yet implemented")
+    # @unittest.skip("Skipping testing part B with demo data, not yet implemented")
     def test_demo_part_b(self):
         demo_b = Puzzle("demo_data1.txt", "b", 1)
         answer = demo_b.solve()
-        self.assertEqual(-1, answer)
+        self.assertEqual(9, answer)
 
-    @unittest.skip("Skipping testing part B with real data, not yet implemented")
+    # @unittest.skip("Skipping testing part B with real data, not yet implemented")
     def test_part_b(self):
         part_b = Puzzle("test_data.txt", "b", 1)
         answer = part_b.solve()
-        self.assertEqual(-1, answer)
+        self.assertEqual(1905, answer)
 
 
 if __name__ == '__main__':
